da429b16b87552c415b1231ca4b6a30fa378f926
Tuan Tuan LE <tlcong@linagora.com>
2020-08-18 15:13:45 +0700

    #1 make UMD build available on both browsers and Node.js
    For more detail: https://webpack.js.org/configuration/output/\#outputglobalobject

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     library: 'ESNApiClient',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
   module: {
     rules: [{
