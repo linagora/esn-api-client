dcd4e27e3be87b19062d0a01d153a1c91bafa684
Huy TA QUOC <htquoc@linagora.com>
2020-09-09 16:45:13 +0700

    https://github.com/OpenPaaS-Suite/esn-api-client/issues/12: Grouped test cases

--- test/Client.spec.js
+++ test/Client.spec.js
@@ -4,53 +4,57 @@ import httpClient from '../src/http-client';
 jest.mock('../src/http-client');
 
 describe('The Client class', () => {
-  test('should throw an error if there is no given baseURL', function() {
-    expect(() => new Client()).toThrow(/baseURL is required/);
+  describe('The constructor', () => {
+    test('should throw an error if there is no given baseURL', () => {
+      expect(() => new Client()).toThrow(/baseURL is required/);
+    });
+
+    test('should pass the given auth param when initializing http client', () => {
+      const config = {
+        baseURL: 'http://esn/api',
+        auth: {
+          type: 'basic',
+          username: 'test',
+          password: 'secret'
+        }
+      };
+
+      httpClient.mockResolvedValue();
+
+      new Client(config); // eslint-disable-line
+
+      expect(httpClient).toHaveBeenCalledWith(config);
+    });
   });
 
-  test('should pass the given auth param when initializing http client', () => {
-    const config = {
-      baseURL: 'http://esn/api',
-      auth: {
-        type: 'basic',
-        username: 'test',
-        password: 'secret'
-      }
-    };
+  describe('The api method', () => {
+    test('should send request with credentials by default', (done) => {
+      const client = new Client({ baseURL: 'http://esn/api' });
+      const config = { foo: 'bar' };
 
-    httpClient.mockResolvedValue();
+      client.httpClient = jest.fn().mockResolvedValue(Promise.resolve({}));
+      client.api(config)
+        .then(() => {
+          expect(client.httpClient).toHaveBeenCalledWith({
+            foo: 'bar',
+            withCredentials: true
+          });
+          done();
+        })
+        .catch((err) => done(err || new Error('should resolve')));
+    });
 
-    new Client(config); // eslint-disable-line
+    test('should send request without credentials if it is intended', (done) => {
+      const client = new Client({ baseURL: 'http://esn/api' });
+      const config = { foo: 'bar', withCredentials: false };
 
-    expect(httpClient).toHaveBeenCalledWith(config);
-  });
-
-  test('should send request with credentials by default', function(done) {
-    const client = new Client({ baseURL: 'http://esn/api' });
-    const config = { foo: 'bar' };
-
-    client.httpClient = jest.fn().mockResolvedValue(Promise.resolve({}));
-    client.api(config)
-      .then(() => {
-        expect(client.httpClient).toHaveBeenCalledWith({
-          foo: 'bar',
-          withCredentials: true
-        });
-        done();
-      })
-      .catch((err) => done(err || new Error('should resolve')));
-  });
-
-  test('should send request without credentials if it is intended', function(done) {
-    const client = new Client({ baseURL: 'http://esn/api' });
-    const config = { foo: 'bar', withCredentials: false };
-
-    client.httpClient = jest.fn().mockResolvedValue(Promise.resolve({}));
-    client.api(config)
-      .then(() => {
-        expect(client.httpClient).toHaveBeenCalledWith(config);
-        done();
-      })
-      .catch((err) => done(err || new Error('should resolve')));
+      client.httpClient = jest.fn().mockResolvedValue(Promise.resolve({}));
+      client.api(config)
+        .then(() => {
+          expect(client.httpClient).toHaveBeenCalledWith(config);
+          done();
+        })
+        .catch((err) => done(err || new Error('should resolve')));
+    });
   });
 });
